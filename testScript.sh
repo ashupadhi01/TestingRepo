6637a5455056469bf99787bcd5fcb90403c5c8be
ashupadhi01 <ashutoshpadhi220@gmail.com>
2024-02-16 21:25:58 +0530

    21:25

--- testScript.sh
+++ testScript.sh
@@ -1,17 +1,10 @@
-echo -e '\n1. What is in this directory ?\n'
-ls -a
+#!/bin/bash
 
-echo -e '\n2. What is the version of Java ?\n'
-java --version
+a='apple'
+b='elephant'
 
-echo -e '\n3. What is the version of gcc ?\n'
-gcc --version
-
-echo -e '\n4. What is the workspace location ?\n'
-echo $RUNNER_WORKSPACE
-
-echo -e '\n5. What environment variables are available ?\n'
-env
-
-echo -e '\n6. Who is the user ?\n'
-whoami
+if [[ "$a" == "$b" ]]; then
+   exit 0
+else 
+   exit 1
+fi
