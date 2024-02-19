627a7987da7192137a4a95cef010fb46d67e3008
ashupadhi01 <ashutoshpadhi220@gmail.com>
2024-02-19 16:48:35 +0530

    checkThisOut

--- forcePush.sh
+++ forcePush.sh
@@ -3,19 +3,7 @@
 oldCommitId=$(git rev-parse HEAD)
 currentTime=$(date +"%H:%M")
 
+git branch "$currentTime"
 git add -A
 git commit -m "$currentTime"
-git push origin main
-
-sleep 5
-
-newCommitId=$(git rev-parse HEAD)
-
-# echo "$oldCommitId"
-# echo "$newCommitId"
-
-if [[ "$oldCommitId" == "$newCommitId" ]]; then
-    echo "Test failed."
-else 
-    echo "Successfully merged"
-fi
+git push origin "$currentTime"
